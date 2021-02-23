bbcee163cbbd731b41d36439f5a7fda6ad67862d
Marco <marcojoel25@gmail.com>
2021-02-23 07:15:04 -0500

    Main code template and js file

---
+++ js/game.js
@@ -0,0 +1,10 @@
+function start() {
+    var canvas = document.getElementById('canvas');
+    var context = document.getContext('2d');
+    canvas.width = 500;
+    canvas.height = 500;
+    context.fillStyle='black';
+    context.fill();
+    
+    
+}
